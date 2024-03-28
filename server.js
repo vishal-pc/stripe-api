05cbd4673207153b38fc3f74d246673378c1cbc3
vishal-pc <vishal.kumar@technocratshorizons.com>
2024-03-28 16:46:31 +0530

    add local changes

--- server.js
+++ server.js
@@ -8,8 +8,6 @@ import stripePackage from "stripe";
 const app = express();
 const port = envConfig.PORT;
 const stripe = stripePackage(envConfig.SECRET_KEY);
-// const endpointSecret =
-//   "whsec_bd1b77afa3459ff996cb4e872a05eaba0b8781099825aec385939d201f279e7e";
 
 app.use(express.json());
 app.use(bodyParser.json());
@@ -21,55 +19,11 @@ app.use(cors({ origin: "*", methods: "GET, POST, PUT, DELETE" }));
 
 app.use("/", userRoutes);
 
-// app.post(
-//   "/webhook",
-//   bodyParser.raw({ type: "application/json" }),
-//   (req, res) => {
-//     handleStripeWebhook(req, res);
-//   }
-// );
-
-// const handleStripeWebhook = async (req, res) => {
-//   console.log("Received webhook request:", req.body);
-//   const sig = req.headers["stripe-signature"];
-
-//   let event;
-
-//   try {
-//     console.log("Enter the try block ==> ");
-//     event = stripe.webhooks.constructEvent(req.body, sig, endpointSecret);
-//     console.log("Received webhook event:", event);
-//   } catch (err) {
-//     return res.status(400).send(`Webhook Error: ${err.message}`);
-//   }
-//   switch (event.type) {
-//     case "payment_method.attached":
-//       const paymentMethodAttached = event.data.object;
-//       console.log("PaymentIntent attached ==>", paymentMethodAttached);
-//       break;
-//     case "payment_method.card_automatically_updated":
-//       const paymentMethodCardAutomaticallyUpdated = event.data.object;
-//       console.log(
-//         "PaymentMethod method updated ==>",
-//         paymentMethodCardAutomaticallyUpdated
-//       );
-//       break;
-//     case "payment_method.detached":
-//       const paymentMethodDetached = event.data.object;
-//       console.log("Payment detached ==>", paymentMethodDetached);
-//       break;
-//     default:
-//       console.log(`Unhandled event type: ${event.type}`);
-//   }
-
-//   response.send();
-// };
-
 app.post(
   "/webhooks",
   bodyParser.raw({ type: "application/json" }),
   async (req, res) => {
-    let signingSecret = "whsec_DczDhixKHy9cVhqCpOEL4n5QBPaszYvD";
+    const signingSecret = envConfig.WEB_HOOK;
     const payload = req.body;
     const signature = req.headers["stripe-signature"];
     let event;
@@ -78,10 +32,10 @@ app.post(
     } catch (err) {
       return res.status(400).send(`Webhook Error: ${err.message}`);
     }
-    console.log("Event type ==>", event.type);
-    console.log("Event data ==>", event.data.object);
-    console.log("Event id ==>", event.data.object.id);
-    res.json({ success: true });
+    // console.log("Event type ==>", event.type);
+    // console.log("Event data ==>", event.data.object);
+    // console.log("Event id ==>", event.data.object.id);
+    return res.json({ success: true });
   }
 );
 app.listen(port, () => {
