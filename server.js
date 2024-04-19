c57ee8b88fd5f255bbc0ce1ff0b6f25e9d863ead
vishal-pc <vishal.kumar@technocratshorizons.com>
2024-04-19 15:34:54 +0530

    new chages

--- server.js
+++ server.js
@@ -7,22 +7,10 @@ import Stripe from "stripe";
 
 const app = express();
 const port = envConfig.PORT;
-
-app.use(express.json());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
-app.use(express.urlencoded({ extended: false }));
-app.set("view engine", "ejs");
-app.use(cors({ origin: "*", methods: "GET, POST, PUT, DELETE" }));
-
-app.use("/", userRoutes);
-
 const endpointSecret = envConfig.WEB_HOOK;
 const stripe = new Stripe(envConfig.SECRET_KEY);
-
 app.post(
-  "/webhooks",
+  "/webhook",
   bodyParser.raw({ type: "application/json" }),
   async (req, res) => {
     let session = "";
@@ -49,13 +37,23 @@ app.post(
           console.log(`Unhandled event type: ${event.type}`);
       }
 
-      res.json({ received: true });
+      res.send();
     } catch (err) {
       console.error("Error processing Stripe webhook:", err);
       res.status(400).send(`Webhook Error: ${err.message}`);
     }
   }
 );
+
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
+app.use(cors({ origin: "*", methods: "GET, POST, PUT, DELETE" }));
+
+app.use("/", userRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running... 🚀`);
